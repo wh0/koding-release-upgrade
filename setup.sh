16c7819ff13b0f78310b0e837f3163f398f7edb5
w <none>
2014-06-21 21:56:11 -0700

    rm broken files

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ apt-mark showmanual | xargs apt-mark auto
 apt-mark manual ldap-auth-client openssh-akc-server python-apt screen sudo ubuntu-minimal ubuntu-standard
 apt-get -y -o Dpkg::Options::=--force-unsafe-io -o Apt::AutoRemove::SuggestsImportant=false autoremove --purge
 sed -i 's/apt-mirror.in.koding.com/us.archive.ubuntu.com/g' /etc/apt/sources.list
+rm /sbin/agetty /usr/share/man/man8/agetty.8.gz /bin/uncompress /bin/dnsdomainname /bin/domainname /bin/bzip2 /usr/sbin/uuidd
 do-release-upgrade -f DistUpgradeViewNonInteractive
 apt-get -y -o Dpkg::Options::=--force-unsafe-io install curl emacs24-nox git zip
